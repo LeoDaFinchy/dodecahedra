e042eca91a67691df7e979d613c8d358e7b97d5b
Adam Finch <leodafinchy@gmail.com>
2013-06-19 08:41:36 -0400

    Removed key and axis from globals.js

--- globals.js
+++ globals.js
@@ -38,24 +38,5 @@ app.globals = {
     this.trace.push(function(){return "Y2:"+app.globals.axes.Y2;});
     this.trace.push(function(){return "Z1:"+app.globals.axes.Z1;});
   },
-  keys:{
-    left:{code:37,state:0},
-    up:{code:38,state:0},
-    right:{code:39,state:0},
-    down:{code:40,state:0},
-    w:{code:87,state:0},
-    a:{code:65,state:0},
-    s:{code:83,state:0},
-    d:{code:68,state:0},
-    e:{code:69,state:0},
-    q:{code:81,state:0},
-  },
-  axes:{
-    X1:0,
-    X2:0,
-    Y1:0,
-    Y2:0,
-    Z1:0
-  },
   trace:[]
 };
